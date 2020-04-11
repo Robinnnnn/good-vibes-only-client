3bdfd710bd97728f4aa504d410a3c419ded1962e
Robin Kim <robin.kim@coinbase.com>
2020-04-11 00:23:22 -0400

    SWRProvider

---
+++ src/contexts/SWR/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { SWRConfig } from 'swr'
+import { useSpotifyState } from '../Spotify'
+import log from '../../util/log'
+import SpotifyWebApi from 'spotify-web-api-js'
+
+// Have to use `any` here because Spotify's interface doesn't follow a generic pattern
+type SpotifyRequestMethod = (...args: any) => any
+
+// Sends authenticated requests via Spotify SDK
+export const AuthenticatedSpotifySWRProvider: React.FC = ({ children }) => {
+  log.info('initializing spotify SWR provider')
+  const { sdk } = useSpotifyState()
+  const value = {
+    fetcher: (action: keyof SpotifyWebApi.SpotifyWebApiJs, ...args) => {
+      log.trace(`calling ${action}`, args)
+      const fn = sdk[action] as SpotifyRequestMethod;
+      return fn(...args)
+    },
+    refreshInterval: 1000 * 60 * 5 // refresh data every 5 minutes    
+  }
+  return <SWRConfig value={value}>{children}</SWRConfig>
+}
--- src/scenes/index.tsx
+++ src/scenes/index.tsx
@@ -4,6 +4,7 @@ import Login from './Login'
 import Playlist from './Playlist'
 import NotFound from './NotFound'
 import { useAuthState, LoggedOutState } from '../contexts/Auth'
+import { AuthenticatedSpotifySWRProvider } from '../contexts/SWR'
 
 const Routes: React.FC = () => {
   const state = useAuthState()
@@ -19,14 +20,16 @@ const Routes: React.FC = () => {
 
   // TODO: cleanup redirects
   return (
-    <Router>
-      <Playlist path='playlist/:id' />
-      <NotFound path='404' />
-      <Redirect noThrow from='/' to='/playlist/37i9dQZF1DXcBWIGoYBM5M' />
-      <Redirect noThrow from='login' to='/playlist/37i9dQZF1DXcBWIGoYBM5M' />
-      <Redirect noThrow from='oauth' to='/playlist/37i9dQZF1DXcBWIGoYBM5M' />
-      <Redirect noThrow from='*' to='404' />
-    </Router>
+    <AuthenticatedSpotifySWRProvider>
+      <Router>
+        <Playlist path='playlist/:id' />
+        <NotFound path='404' />
+        <Redirect noThrow from='/' to='/playlist/37i9dQZF1DXcBWIGoYBM5M' />
+        <Redirect noThrow from='login' to='/playlist/37i9dQZF1DXcBWIGoYBM5M' />
+        <Redirect noThrow from='oauth' to='/playlist/37i9dQZF1DXcBWIGoYBM5M' />
+        <Redirect noThrow from='*' to='404' />
+      </Router>
+    </AuthenticatedSpotifySWRProvider>
   )
 }
 

b3108c121e133d3c65df0f21e781d21a0471393d
Robin <robin.kim@coinbase.com>
2020-12-25 02:35:15 -0500

    slightly improve loading experience with queue, though still a bit jank

--- src/contexts/ImageLoader/ImageLoaderContext.tsx
+++ src/contexts/ImageLoader/ImageLoaderContext.tsx
@@ -25,7 +25,13 @@ export const ImageWithSuspense = ({
 const MAX_WAIT_TIME = 5000
 
 export const ImageLoaderProvider: React.FC = ({ children }) => {
+  // a map to look up the load status for each image
   const cache = React.useRef<{ [url: string]: Promise<boolean> | boolean }>({})
+
+  // a queue to keep track of unloaded images. the useEffect will consume
+  // from this queue as to not bombard the client with a bunch of parallelized
+  // image requests, which causes the loading gif to lag
+  const [queue, setQueue] = React.useState([])
 
   const read = React.useCallback((src: string) => {
     if (cache.current[src] instanceof Promise) {
@@ -34,23 +40,34 @@ export const ImageLoaderProvider: React.FC = ({ children }) => {
       return cache.current[src]
     }
 
-    const promise = new Promise((resolve) => {
-      const img = new Image()
-      img.src = src
-      const _id = setTimeout(() => resolve(src), MAX_WAIT_TIME)
-      img.onload = () => {
-        resolve(src)
-        clearTimeout(_id)
-      }
-    })
-      .then(() => (cache.current[src] = true))
-      // resolve anyway, users can deal with errors lol
-      .catch(() => (cache.current[src] = true))
-
+    const promise = new Promise<boolean>((resolve) => resolve(true))
     cache.current[src] = promise
-
-    return promise
+    setQueue((_toLoad) => [..._toLoad, src])
+    throw promise
   }, [])
+
+  React.useEffect(() => {
+    if (queue.length) {
+      const src = queue[0]
+
+      new Promise((resolve) => {
+        const img = new Image()
+        img.src = src
+        const _id = setTimeout(() => resolve(src), MAX_WAIT_TIME)
+        img.onload = () => {
+          // if necessary, use a timeout to space out the load even further.
+          // setTimeout(() => {
+          setQueue((_urls) => _urls.slice(1))
+          // }, 10)
+          resolve(src)
+          clearTimeout(_id)
+        }
+      })
+        .then(() => (cache.current[src] = true))
+        // resolve anyway, users can deal with errors lol
+        .catch(() => (cache.current[src] = true))
+    }
+  }, [queue])
 
   const actions: ImageLoaderActions = React.useMemo(() => ({ read }), [read])
 
